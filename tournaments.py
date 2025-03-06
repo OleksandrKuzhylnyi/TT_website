a6b832ca2636a9e3fda0923f81fcf3bd0e5eab73
Oleksandr Kuzhylnyi <oleksandrkuzhylnyi@gmail.com>
2025-03-06 19:43:48 +0200

    Improved plotting of graphs

--- tournaments.py
+++ tournaments.py
@@ -13,16 +13,19 @@ def plot_num_of_players(df):
     num_of_players_L = late.groupby("date")["place"].max()
 
     plt.figure(figsize=(12, 6))
-    plt.plot(num_of_players_E.index, num_of_players_E.values, marker='o', label="Early Tournament", color='blue')
-    plt.plot(num_of_players_L.index, num_of_players_L.values, marker='s', label="Late Tournament", color='red')
+    plt.plot(num_of_players_E.index, num_of_players_E.values, marker='o', markersize=5,
+             label="Early Tournament", color='blue')
+    plt.plot(num_of_players_L.index, num_of_players_L.values, marker='s', markersize=5, 
+             label="Late Tournament", color='red')
     plt.xlabel("Date")
     plt.ylabel("Number of Players")
     plt.title("Number of Players in Titled Tuesday (Early vs. Late)")
-    plt.xticks(rotation=45)
+    plt.xticks(num_of_players_E.index[::2], rotation=45)
     plt.legend()
-    plt.grid(True)
-
-    plt.savefig("static/max_players.png")
+    plt.grid(True, linestyle='--', linewidth=0.5, alpha=0.7)
+    plt.tight_layout()
+    plt.gca().set_facecolor('#f9f9f9')
+    plt.savefig("static/num_of_players.png")
     plt.close()
 
 def average_score_of_winner(df):
@@ -35,13 +38,14 @@ def average_score_of_winner(df):
     plt.plot(early_scores["date"], early_scores["score"], label="Early", marker='o', color='blue')
     plt.plot(late_scores["date"], late_scores["score"], label="Late", marker='s', color='red')
     plt.legend()
-    plt.xticks(rotation=45)
+    plt.xticks(early_scores["date"][::2], rotation=45)
     plt.yticks(np.arange(9.5, 11.5, 0.5))
     plt.title("Score of the Winner over Time")
     plt.xlabel("Date")
     plt.ylabel("Score")
-    plt.grid(True)
-
+    plt.grid(True, linestyle='--', linewidth=0.5, alpha=0.7)
+    plt.tight_layout()
+    plt.gca().set_facecolor('#f9f9f9')
     plt.savefig("static/winner_score.png")
     plt.close()
 
@@ -57,13 +61,14 @@ def average_score_of_top_10(df):
     plt.plot(early_scores["date"], early_scores["score"], label="Early", marker='o', color='blue')
     plt.plot(late_scores["date"], late_scores["score"], label="Late", marker='s', color='red')
     plt.legend()
-    plt.xticks(rotation=45)
+    plt.xticks(early_scores["date"][::2], rotation=45)
     plt.yticks(np.arange(8.5, 10.5, 0.5))
     plt.title("Score of the Winner over Time")
     plt.xlabel("Date")
     plt.ylabel("Score")
-    plt.grid(True)
-
+    plt.grid(True, linestyle='--', linewidth=0.5, alpha=0.7)
+    plt.tight_layout()
+    plt.gca().set_facecolor('#f9f9f9')
     plt.savefig("static/top_10_score.png")
     plt.close()
 
@@ -79,13 +84,14 @@ def average_rating_of_top_10(df):
     plt.plot(early_rating["date"], early_rating["rating"], label="Early", marker='o', color='blue')
     plt.plot(late_rating["date"], late_rating["rating"], label="Late", marker='s', color='red')
     plt.legend()
-    plt.xticks(rotation=45)
+    plt.xticks(early_rating["date"][::2], rotation=45)
     plt.yticks(np.arange(2950, 3200, 50))
     plt.title("Average Rating of Top 10 over Time")
     plt.xlabel("Date")
     plt.ylabel("Rating")
-    plt.grid(True)
-
+    plt.grid(True, linestyle='--', linewidth=0.5, alpha=0.7)
+    plt.tight_layout()
+    plt.gca().set_facecolor('#f9f9f9')
     plt.savefig("static/top_10_rating.png")
     plt.close()
 
@@ -93,13 +99,13 @@ def skips_per_round(df):
     num_games = df.shape[0]
     skips = [(df[f"round_{i}"] == "U--").sum() / num_games for i in range(1, 12)]
     fig = plt.figure(figsize=(12, 6))
-    plt.bar(np.arange(1, 12), skips, color='blue')
+    plt.bar(np.arange(1, 12), skips, color='royalblue', edgecolor='black', alpha=0.8)
+    for i, v in enumerate(skips):
+        plt.text(i + 1, v + 0.005, f"{v:.1%}", ha='center', fontsize=10, color='black')
     plt.xticks(np.arange(1, 12))
     plt.title("Percent of Skips per Round")
     plt.xlabel("Round")
-    plt.ylabel("Percent of Skips")
-    plt.gca().yaxis.set_major_formatter(PercentFormatter(1))
-
+    plt.yticks([])
     plt.savefig("static/skips_per_round.png")
     plt.close()
 
@@ -112,7 +118,9 @@ def starting_ranks_of_winners(df):
 
     plt.figure(figsize=(8, 8))
     plt.pie(category_counts, labels=category_counts.index, autopct="%1.1f%%", startangle=140, colors=plt.cm.Paired.colors)
-    plt.title("Proportion of Winners by Starting Position")
+    plt.title("Proportion of Winners by Starting Rank")
 
     plt.savefig("static/starting_ranks.png")
     plt.close()
+
+starting_ranks_of_winners(pd.read_csv("static/data.csv"))
